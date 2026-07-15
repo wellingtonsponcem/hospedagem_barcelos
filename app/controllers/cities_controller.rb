356bc607452a31a9b1044eb0502befc4bf49bc9c
lindavoz <soulindavoz@gmail.com>
2026-07-14 23:26:00 -0300

    Fix NoMethodError in CitiesController by using safe navigation dig for nested IBGE API properties

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -7,7 +7,12 @@ class CitiesController < ApplicationController
       uri = URI("https://servicodados.ibge.gov.br/api/v1/localidades/municipios")
       response = Net::HTTP.get(uri)
       data = JSON.parse(response)
-      data.map { |c| { cidade: c["nome"], uf: c["microrregiao"]["mesorregiao"]["UF"]["sigla"] } }
+      data.map do |c|
+        uf = c.dig("microrregiao", "mesorregiao", "UF", "sigla") || 
+             c.dig("regiao-imediata", "regiao-intermediaria", "UF", "sigla") || 
+             "UF"
+        { cidade: c["nome"], uf: uf }
+      end
     end
     render json: cities
   end
